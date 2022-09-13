8fc6c3ebffa46af5e063eaddad7d6a78d9351655
Prathamesh Karambelkar <83802999+prathameshkarambelkar@users.noreply.github.com>
2022-09-14 00:19:43 +0530

    props understanding

--- src/App.js
+++ src/App.js
@@ -1,33 +1,47 @@
 
-const author = 'James Clear'
-const title = 'Atomic Habits: The life-changing million copy bestseller'
-const img = 'https://images-na.ssl-images-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg'
+const secondBook = [
+  {
+  img: 'https://images-eu.ssl-images-amazon.com/images/I/51-nXsSRfZL._AC_SX184_.jpg',
+  title: 'Atomic Habits: The life-changing million copy bestseller',
+  author: 'James Clear',
+  },
+  {
+  img: 'https://images-eu.ssl-images-amazon.com/images/I/51xwGSNX-EL._AC_SX184_.jpg',
+  title: 'Ikigai: The Japanese secret to a long and happy life',
+  author: 'Héctor García',
+  }
+];
+
+const names = ['john','pk','sonw']
+
 function BookList() {
   return <section className="booklist">
   <Book />
-  <Book />
-  <Book />
-  <Book />
-  <Book />
-  <Book />
-  <Book />
   </section>;
 }
+// const Book = ({img,title,author})=>{
+// const Book = (props)=>{
 
-const Book = () => {
+// const {img,title,author} = props;}
+
+
+// }
+
+
+const Book = (props) => {
   return <article className="book">
-  <img src={img} alt="Book Image" />
-  <h1>{title}</h1>
-  <h4>{author}</h4>
+    <img src={props.img} alt="" />
+    <h1>{props.title}</h1>
+    <h4>{props.author}</h4>
 
- </article>;
+  </article>;
 }
 
 
 
 function App() {
   return (
-    <BookList/>
+    <BookList />
   );
 }
 
