ea6f9e0739a290d390c2bb9a6b96b158f34309ce
Prathamesh Karambelkar <83802999+prathameshkarambelkar@users.noreply.github.com>
2022-09-14 15:03:04 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 
-const secondBook = [
+const books = [
   {
-  img: 'https://images-eu.ssl-images-amazon.com/images/I/51-nXsSRfZL._AC_SX184_.jpg',
-  title: 'Atomic Habits: The life-changing million copy bestseller',
-  author: 'James Clear',
+    id: 1,
+    img: 'https://images-eu.ssl-images-amazon.com/images/I/51-nXsSRfZL._AC_SX184_.jpg',
+    title: 'Atomic Habits: The life-changing million copy bestseller',
+    author: 'James Clear',
   },
   {
-  img: 'https://images-eu.ssl-images-amazon.com/images/I/51xwGSNX-EL._AC_SX184_.jpg',
-  title: 'Ikigai: The Japanese secret to a long and happy life',
-  author: 'Héctor García',
-  }
+    id: 2,
+    img: 'https://images-eu.ssl-images-amazon.com/images/I/51xwGSNX-EL._AC_SX184_.jpg',
+    title: 'Ikigai: The Japanese secret to a long and happy life',
+    author: 'Héctor García',
+  },
+  {
+    id: 3,
+    img: 'https://images-eu.ssl-images-amazon.com/images/I/41r6F2LRf8L._AC_SX184_.jpg',
+    title: 'The Psychology of Money',
+    author: 'Morgan Housel',
+  },
 ];
 
-const names = ['john','pk','sonw']
 
 function BookList() {
-  return <section className="booklist">
-  <Book />
-  </section>;
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book  key={books.id} {...book}></Book>;
+      })}
+    </section>
+  );
 }
+
+
 // const Book = ({img,title,author})=>{
 // const Book = (props)=>{
 
@@ -28,13 +41,28 @@ function BookList() {
 // }
 
 
-const Book = (props) => {
-  return <article className="book">
-    <img src={props.img} alt="" />
-    <h1>{props.title}</h1>
-    <h4>{props.author}</h4>
+const Book = ({ img, title, author }) => {
+  // const { img, title, author } = props.book;
+  // attribute , eventHandler
+  // onClick , onMouseOver
 
-  </article>;
+  // const clickHandler = () => {
+  //   alert('Hello World');
+  // }
+
+  // const complexExample = (author) => {
+  //   alert("author");
+
+  // }
+
+  return ( <article className="book">
+    <img src={img} alt="" />
+    <h1>{title}</h1>
+    <h4>{author}</h4>
+    {/* <button type="button" onClick={clickHandler} > Reference Example</button>
+    <button type="button" onClick={ () => complexExample() } > More complex example</button> */}
+  </article>
+  );
 }
 
 
